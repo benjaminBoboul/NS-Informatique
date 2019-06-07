8221fdf247e036e46945d185b4003659f6923569
Boboul Benjamin <benjamin.boboul@orange.com>
2019-06-07 11:46:42 +0200

    Starting writing primary functions

--- src/bookArray.c
+++ src/bookArray.c
@@ -3,36 +3,59 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct BookArray {
+    IBook[10] *books;
+    IBookArray *next;
+};
+
 IBookArray bookaNew() {
-	return NULL;
+    IBookArray this = calloc(1, sizeof(struct BookArray)); // TODO: rajouter un espace pour un pointeur
+    this->books = null;
+    this->next = null;
+    return this;
 }
 
 void bookaDelete(IBookArray this) {
+    free(this);
 }
 
 void bookaAppend(IBookArray this, IBook book) {
+    this->books[0] = book;
 }
 
 IBook bookaGet(IBookArray this, int i) {
-	return NULL;
+	return this->books[i];
 }
 
 int bookaIndexOf(IBookArray this, IBook book) {
-	return 0;
+    int i = 0;
+    while (this->books[i] != book) {i++;}
+	return i;
 }
 
 void bookaInsertAt(IBookArray this, int i, IBook book) {
+    this->books[i] = book;
 }
 
 void bookaRemoveAt(IBookArray this, int i) {
+    // [book1, book2, book3]
+    this->books[i] = null;
 }
 
 void bookaRemoveLast(IBookArray this) {
+    if(bookaSize(this) != -1) this->books[bookaSize(this)] = null;
 }
 
 void bookaSet(IBookArray this, int i, IBook book) {
+    this->books[i] = book;
 }
 
 int bookaSize(IBookArray this) {
-	return 0;
+    int i = 0;
+    while(this[i] != '\0') {i++;}
+	return i;
+}
+
+void grow(IBookArray this) {
+    this->next = bookaNew()
 }
